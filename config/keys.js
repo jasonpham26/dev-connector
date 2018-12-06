ddeccad79e99926c9f91ba2efd8d160308eb072d
jasonpham26 <vinhphuong0206@gmail.com>
2018-12-06 22:44:43 +1000

    Added model and Login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://jason:39Sunview@ds123584.mlab.com:23584/dev-connector'
+  mongoURI: 'mongodb://jason:39Sunview@ds123584.mlab.com:23584/dev-connector',
+  secretKey: 'secret'
 };
