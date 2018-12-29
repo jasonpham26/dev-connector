11a3095abfde743ba0ea1a16d5ee8bc3981b711b
jasonpham26 <vinhphuong0206@gmail.com>
2018-12-29 22:47:23 +1000

    Finished

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://jason:39Sunview@ds123584.mlab.com:23584/dev-connector',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
